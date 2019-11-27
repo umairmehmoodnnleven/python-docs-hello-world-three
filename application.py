8d8b7193afd6c4e69bae7aeed2b74a1e5f53a9d8
umairmehmoodnn <58176512+umairmehmoodnn@users.noreply.github.com>
2019-11-27 14:54:42 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,22 +1,40 @@
 # from flask import Flask
+#app = Flask(__name__)
+
+#@app.route("/hello/")
+#def hello():
+#    return "Hello World How are you doing? Test 1 2 3!"
+#
 
 from dash import Dash
 from werkzeug.wsgi import DispatcherMiddleware
 import flask
+from werkzeug.serving import run_simple
 import dash_html_components as html
 
-server = Flask(__name__)
+server = flask.Flask(__name__)
 dash_app1 = Dash(__name__, server = server, url_base_pathname='/dashboard/' )
+dash_app2 = Dash(__name__, server = server, url_base_pathname='/reports/')
 dash_app1.layout = html.Div([html.H1('Hi there, I am app1 for dashboards')])
-
+dash_app2.layout = html.Div([html.H1('Hi there, I am app2 for reports')])
 @server.route('/')
-@server.route("/hello/")
+@server.route('/hello/')
 def hello():
-    return "Hello World How are you doing? Test 1 2 3!"
+    return 'hello world!'
 
 @server.route('/dashboard/')
 def render_dashboard():
     return flask.redirect('/dash1/')
 
+
+@server.route('/reports/')
+def render_reports():
+    return flask.redirect('/dash2/')
+
 app = DispatcherMiddleware(server, {
-    '/dash1/': dash_app1.server})
+    '/dash1/': dash_app1.server,
+    '/dash2/': dash_app2.server
+})
+
+#if __name__ == '__main__':
+#    run_simple('localhost', 80, app, use_reloader=True, use_debugger=True)
