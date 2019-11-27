a3b4d58ff80f7928fd36a12ebd964fbd423cae83
umairmehmoodnn <58176512+umairmehmoodnn@users.noreply.github.com>
2019-11-27 12:07:35 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,22 @@
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
 
-@app.route("/hello/")
+from dash import Dash
+from werkzeug.wsgi import DispatcherMiddleware
+import flask
+import dash_html_components as html
+
+server = Flask(__name__)
+dash_app1 = Dash(__name__, server = server, url_base_pathname='/dashboard/' )
+dash_app1.layout = html.Div([html.H1('Hi there, I am app1 for dashboards')])
+
+@server.route('/')
+@server.route("/hello/")
 def hello():
     return "Hello World How are you doing? Test 1 2 3!"
+
+@server.route('/dashboard/')
+def render_dashboard():
+    return flask.redirect('/dash1/')
+
+app = DispatcherMiddleware(server, {
+    '/dash1/': dash_app1.server})
