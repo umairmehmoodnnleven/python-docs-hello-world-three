5f58591b030725437cf49d4ef5da3345b7f0a138
umairmehmoodnn <58176512+umairmehmoodnn@users.noreply.github.com>
2019-11-27 15:08:28 +0100

    Update application.py

--- application.py
+++ application.py
@@ -37,5 +37,5 @@ app = DispatcherMiddleware(server, {
 })
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run()
     #run_simple('localhost', 80, app, use_reloader=True, use_debugger=True)
