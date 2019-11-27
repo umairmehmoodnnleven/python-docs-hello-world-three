b088519328255367eb804a276257943f8ed55144
umairmehmoodnn <58176512+umairmehmoodnn@users.noreply.github.com>
2019-11-27 16:03:34 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,26 +1,26 @@
-# from flask import Flask
-#app = Flask(__name__)
+from flask import Flask
+app = Flask(__name__)
 
-#@app.route("/hello/")
-#def hello():
-#    return "Hello World How are you doing? Test 1 2 3!"
-#
-
-from dash import Dash
-from werkzeug.wsgi import DispatcherMiddleware
-import flask
-from werkzeug.serving import run_simple
-import dash_html_components as html
-
-server = flask.Flask(__name__)
-dash_app1 = Dash(__name__, server = server, url_base_pathname='/dashboard/' )
-dash_app2 = Dash(__name__, server = server, url_base_pathname='/reports/')
-dash_app1.layout = html.Div([html.H1('Hi there, I am app1 for dashboards')])
-dash_app2.layout = html.Div([html.H1('Hi there, I am app2 for reports')])
-@server.route('/')
-@server.route('/hello/')
+@app.route("/hello/")
 def hello():
-    return 'hello world!'
+    return "Hello World How are you doing? Test 1 2 3!"
+
+
+#from dash import Dash
+#from werkzeug.wsgi import DispatcherMiddleware
+#import flask
+#from werkzeug.serving import run_simple
+#import dash_html_components as html
+
+#server = flask.Flask(__name__)
+#dash_app1 = Dash(__name__, server = server, url_base_pathname='/dashboard/' )
+#dash_app2 = Dash(__name__, server = server, url_base_pathname='/reports/')
+#dash_app1.layout = html.Div([html.H1('Hi there, I am app1 for dashboards')])
+#dash_app2.layout = html.Div([html.H1('Hi there, I am app2 for reports')])
+#@server.route('/')
+#@server.route('/hello/')
+#def hello():
+#    return 'hello world!'
 
 #@server.route('/dashboard/')
 #def render_dashboard():
