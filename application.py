7b680e496031a4df4ec9704c5970b1e94f19b0de
umairmehmoodnn <58176512+umairmehmoodnn@users.noreply.github.com>
2019-11-27 16:10:08 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask
-app = Flask(__name__)
+server = Flask(__name__)
 
-@app.route("/")
+@server.route("/")
 def hello():
     return "Hello World How are you doing? Test 1 2 3!"
 
