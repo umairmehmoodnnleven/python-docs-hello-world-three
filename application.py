1cb666e6af173ea4e3dca1077a503b1d36b2f398
umairmehmoodnn <58176512+umairmehmoodnn@users.noreply.github.com>
2019-11-27 17:27:55 +0100

    Update application.py

--- application.py
+++ application.py
@@ -17,9 +17,9 @@ def test():
 #import dash_html_components as html
 
 #server = flask.Flask(__name__)
-dash_app1 = Dash(__name__, server = app, url_base_pathname='/dashboard/' )
+#dash_app1 = Dash(__name__, server = app, url_base_pathname='/dashboard/' )
 #dash_app2 = Dash(__name__, server = server, url_base_pathname='/reports/')
-dash_app1.layout = html.Div([html.H1('Hi there, I am app1 for dashboards')])
+#dash_app1.layout = html.Div([html.H1('Hi there, I am app1 for dashboards')])
 #dash_app2.layout = html.Div([html.H1('Hi there, I am app2 for reports')])
 #@server.route('/')
 #@server.route('/hello/')
