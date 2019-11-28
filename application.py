2e511ca4919376e07937bca7ed45743a6e54dc1c
umairmehmoodnn <58176512+umairmehmoodnn@users.noreply.github.com>
2019-11-28 11:41:43 +0100

    Update application.py

--- application.py
+++ application.py
@@ -3,6 +3,10 @@ import flask
 from dash import Dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_daq as daq
+import pandas as pd
+import plotly.graph_objs as go
+
 app = flask.Flask(__name__)
 #dash_app1 = Dash(__name__, server = app, url_base_pathname='/dashboard/')
 #dash_app1.layout = html.Div([html.H1('Hi there, I am app1 for dashboards')])
@@ -11,28 +15,75 @@ app = flask.Flask(__name__)
 def hello():
     return "Hello World How are you doing? Test 1 2 3!"
 
+colors = {
+    'background': '#1e2130',
+    'text': '#7FDBFF'
+}
+
 @app.route('/test/')
 def test():
-    dash_app1 = Dash(__name__, server = app, url_base_pathname='/dashboard/')
-    dash_app1.layout = html.Div(children=[
-       html.H1(children='Hello Dash'),
+    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-       html.Div(children='''
-           Dash: A web application framework for Python.
-       '''),
-        dcc.Graph(
-           id='example-graph',
-           figure={
-               'data': [
-                   {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                   {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-               ],
-               'layout': {
-                   'title': 'Dash Data Visualization'
-               }
-           }
-       )
-   ])   
+    dash_app1 = Dash(__name__, server = app, external_stylesheets = external_stylesheets, url_base_pathname='/dashboard/')
+    dash_app1.layout = html.Div(style={'backgroundColor': colors['background']}, children =[
+    html.H1("SII-sensitivities", style={"fontWeight": "bold", "textAlign": "center", 'color' : 'orange'}),
+    
+    html.Div([
+        html.Div([
+        html.Div([html.Div([dcc.Dropdown(id='product-selected1',
+                                         options=[{'label': i.title(), 'value': i} for i in df.Scenario.unique()],
+                                         value="Equity -25% MTM")], className="six columns",
+                           style={"width": "40%", "float": "right"}),
+                  html.Div([dcc.Dropdown(id='product-selected2',
+                                         options=[{'label': i.title(), 'value': i} for i in df.Period.unique()],
+                                         value='2019 Q1')], className="six columns", style={"width": "40%", "float": "left"}),
+                  ], className="row", style={"padding": 50, "width": "60%", "margin-left": "auto", "margin-right": "auto"}),
+        dcc.Graph(id='my-graph')
+        ],className = 'eight columns'),
+        
+        
+        html.Div(
+            id="card-2",
+            children=[
+                html.P("Current SII-ratio", style={"textAlign": "center", 'color' : 'orange'}),
+                daq.Gauge(
+                    id="progress-gauge",
+                    max=300,
+                    min=0,
+                    style={"textAlign": "center"},
+                    value = 211,
+                    showCurrentValue=True,  # default size 200 pixel
+                    color = 'orange',),],className = 'four columns', style = {'margin-top':200}),
+    ],className = 'row')
+         
+    # dcc.Link('Go to Source Code', href='{}/code'.format(app_name))
+], className="row")
+    
+    @dash_app1.callback(
+        dash.dependencies.Output('my-graph', 'figure'),
+        [dash.dependencies.Input('product-selected1', 'value'),
+        dash.dependencies.Input('product-selected2', 'value')])
+    def update_graph(selected_product1, selected_product2):
+        #dff = df[(df[selected_product1] >= 2) & (df[selected_product2] >= 2)]
+        df1 = df[((df.Period == selected_product2))]
+        df2 = df1[((df1.Scenario == 'Reported figures'))]
+        df3 = df1[((df1.Scenario == selected_product1))]
+        text = [int(df2['SII-ratio'].values[0]*100), int(df3['SII-ratio'].values[0]*100)]
+        dff = pd.concat([df2, df3])
+        trace1 = go.Bar(x=dff['Scenario'], y=dff['EOF'], name='EOF', )
+        trace2 = go.Bar(x=dff['Scenario'], y=dff['SCR'], name='SCR', )
+
+        return {
+            'data': [trace1, trace2],
+            'layout': go.Layout(title=  f'SII-ratio changes from {text[0]}% to {text[1]}%',
+                                colorway=["#EF963B", "#EF533B"], hovermode="closest",
+                                plot_bgcolor = colors['background'],
+                                paper_bgcolor = colors['background'],
+                                font = {'color' : 'orange'},
+                                xaxis={'title': "Scenario", 'titlefont': {'color': 'orange', 'size': 14},
+                                       'tickfont': {'size': 9, 'color': 'orange'}},
+                                yaxis={'title': "EUR (million)", 'titlefont': {'color': 'orange', 'size': 14, },
+                                       'tickfont': {'color': 'orange'}})}
     
     
     return dash_app1
